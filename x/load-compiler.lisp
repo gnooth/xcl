4d37fc06f79d8a98a026f941dd9f988707898608
Peter Graves <peter@armedbear.org>
2009-08-06 10:16:26 -0700

    *compiler-debug*, debug-log

--- x/load-compiler.lisp
+++ x/load-compiler.lisp
@@ -1,3 +1,21 @@
+;;; load-compiler.lisp
+;;;
+;;; Copyright (C) 2006-2009 Peter Graves <peter@armedbear.org>
+;;;
+;;; This program is free software; you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License
+;;; as published by the Free Software Foundation; either version 2
+;;; of the License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program; if not, write to the Free Software
+;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
 (in-package "SYSTEM")
 
 (require "CLOS")
@@ -29,6 +47,16 @@
                     :use '("COMMON-LISP" "EXTENSIONS" "SYSTEM"
                            #+x86    "X86"
                            #+x86-64 "X86-64"))))
+
+(defvar *compiler-debug* t)
+
+(export '*compiler-debug*)
+
+(defun debug-log (control-string &rest format-arguments)
+  (when *compiler-debug*
+    (apply #'format t control-string format-arguments)))
+
+(export 'debug-log)
 
 (load-system-file "derive-type")
 
