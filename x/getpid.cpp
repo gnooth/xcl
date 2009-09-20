9b5cf4ee6741a799230a197dab73edb8e84d54a2
unknown <peter@.(none)>
2009-09-20 11:13:47 -0700

    getpid (Windows)

--- x/getpid.cpp
+++ x/getpid.cpp
@@ -16,13 +16,19 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
+#ifndef WIN32
 #include <sys/types.h>
 #include <unistd.h>
+#endif
 
 #include "lisp.hpp"
 
 Value EXT_getpid()
 {
+#ifdef WIN32
+  DWORD pid = GetCurrentProcessId();
+#else
   pid_t pid = getpid();
+#endif
   return make_number(pid);
 }
