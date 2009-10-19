a76166b74890f2e5fcb1d7bcd098cce11591185a
Peter Graves <peter@armedbear.org>
2009-10-19 04:49:57 -0700

    p2-constant: support arrays (x86-64, tested)

--- compiler/x86-64/p2-x86-64.lisp
+++ compiler/x86-64/p2-x86-64.lisp
@@ -426,7 +426,7 @@
               (compiler-unsupported "P2-CONSTANT: unsupported target ~S" target))))
           ((or (numberp form)
                (symbolp form)
-               (vectorp form)
+               (arrayp form)
                (listp form)
                (characterp form)
                (hash-table-p form)
