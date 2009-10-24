b4a964f2a7f00e93b8b17ed08ac64458629b9502
Peter Graves <peter@armedbear.org>
2009-10-23 21:41:52 -0700

    fixed indentation

--- lisp/early-macros.lisp
+++ lisp/early-macros.lisp
@@ -17,7 +17,7 @@
 ;;; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 (cl:eval-when (:compile-toplevel :load-toplevel :execute)
-              (sys:%in-package "SYSTEM"))
+  (sys:%in-package "SYSTEM"))
 
 (defmacro in-package (name)
   `(eval-when (:compile-toplevel :load-toplevel :execute)
