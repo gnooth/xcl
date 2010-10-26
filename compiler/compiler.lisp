35d325f5c1c6a33096a782500a8e734d8c3a3a7e
Peter Graves <gnooth@gmail.com>
2010-10-26 05:04:27 -0700

    p1-function: no implicit block for lambda forms (this change breaks load.xcl)

--- compiler/compiler.lisp
+++ compiler/compiler.lisp
@@ -753,7 +753,7 @@
              (multiple-value-bind (body decls)
                  (parse-body body)
                (setf (compiland-lambda-expression compiland)
-                     `(lambda ,lambda-list ,@decls (block nil ,@body)))
+                     `(lambda ,lambda-list ,@decls ,@body))
                (let ((*visible-variables* *visible-variables*)
                      (*local-variables* nil)
                      (*current-compiland* compiland))
