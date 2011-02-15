b3b39b1f3272cf6da042b73a50178cbab943592b
Peter Graves <gnooth@gmail.com>
2011-02-15 15:26:59 -0800

    source.lisp => source-information.cpp

---
+++ kernel/source-information.cpp
@@ -0,0 +1,59 @@
+// source-information.cpp
+//
+// Copyright (C) 2006-2011 Peter Graves <gnooth@gmail.com>
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+#include "lisp.hpp"
+#include "primitives.hpp"
+
+// ### record-source-information
+Value SYS_record_source_information(Value name, Value source_position)
+{
+  if (symbolp(name)) // FIXME support setf functions too
+    {
+      Thread * const thread = current_thread();
+      Value source = thread->symbol_value(S_source_file);
+      if (source != NIL)
+        {
+          if (source_position != NIL)
+            the_symbol(name)->put(S_source_internal, make_cons(source, source_position));
+        }
+    }
+  return T;
+}
+
+// ### source
+Value SYS_source(Value arg)
+{
+  return SYS_get3(arg, S_source_internal, NIL);
+}
+
+// ### source-file-position
+Value SYS_source_file_position(Value arg)
+{
+  Value source = SYS_get3(arg, S_source_internal, NIL);
+//   if (consp(source))
+//     return xcdr(source);
+//   return NIL;
+  return consp(source) ? xcdr(source) : NIL;
+}
+
+// ### source-pathname
+Value SYS_source_pathname(Value arg)
+{
+  Value source = SYS_get3(arg, S_source_internal, NIL);
+  return consp(source) ? xcar(source) : source;
+}
